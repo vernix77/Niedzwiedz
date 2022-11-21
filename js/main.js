ccbcda864646342a4ccb9cc02c8682e12f566bfc
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2022-11-21 12:51:51 +0100

    #7 mobile complete

--- js/main.js
+++ js/main.js
@@ -14,3 +14,39 @@ function PhotoSwap() {
 
 arrow_left.addEventListener("click", PhotoSwap);
 arrow_right.addEventListener("click", PhotoSwap);
+
+const counterProduct = document.querySelectorAll(
+	".recomendations__products-prod-couter-input"
+);
+const counterMinusProduct = document.querySelectorAll(
+	".recomendations__products-prod-minus"
+);
+const counterPlusProduct = document.querySelectorAll(
+	".recomendations__products-prod-plus"
+);
+
+// slajder produktowy
+
+let count = 1;
+
+updateProducts();
+
+for (let i = 0, len = counterPlusProduct.length; i < len; i++) {
+	counterPlusProduct[i].addEventListener("click", () => {
+		counterProduct[i].value++;
+	});
+}
+
+for (let i = 0, len = counterMinusProduct.length; i < len; i++) {
+	counterMinusProduct[i].addEventListener("click", () => {
+		if (counterProduct[i].value > 1) {
+			count = counterProduct[i].value--;
+		}
+	});
+}
+
+function updateProducts() {
+	for (let i = 0, len = counterProduct.length; i < len; i++) {
+		counterProduct[i].value++;
+	}
+}
