bf04e085d98756a8cabafc76ff73a41b3e0cc930
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2022-11-22 10:20:58 +0100

    #4 fix font i #5 slajder poprawki

--- js/main.js
+++ js/main.js
@@ -15,6 +15,68 @@ function PhotoSwap() {
 arrow_left.addEventListener("click", PhotoSwap);
 arrow_right.addEventListener("click", PhotoSwap);
 
+//swipe
+const slider = document.querySelector(".main__banner");
+//add touch-action: pan-y; if browser is Chrome
+
+if (navigator.appVersion.indexOf("Chrome/") != -1) {
+	document.querySelector(".main__banner").style.touchAction = "pan-y";
+}
+
+let xDown = null;
+let yDown = null;
+
+let swiped = false;
+
+slider.addEventListener("touchstart", handleTouchStart, false);
+slider.addEventListener("touchmove", handleTouchMove, false);
+
+function getTouches(event) {
+	return event.touches;
+}
+
+function handleTouchStart(event) {
+	const firstTouch = getTouches(event)[0];
+	xDown = firstTouch.clientX;
+	yDown = firstTouch.clientY;
+	swiped = false;
+}
+
+function handleTouchMove(event) {
+	if (!swiped) {
+		//check if already scrolled
+		if (!xDown || !yDown) {
+			return;
+		}
+
+		let xUp = event.touches[0].clientX;
+		let yUp = event.touches[0].clientY;
+
+		let xDiff = xDown - xUp;
+		let yDiff = yDown - yUp;
+
+		if (Math.abs(xDiff) > Math.abs(yDiff)) {
+			//check if swipe is horizontal
+			if (xDiff > 0) {
+				//check if swipe is left or right
+				PhotoSwap();
+			} else {
+				PhotoSwap();
+			}
+		} else {
+			if (yDiff > 0) {
+				//check if swipe is up or down
+				//console.log('down');
+			} else {
+				//console.log('up');
+			}
+		}
+		swiped = true;
+	}
+}
+
+// slajder produktowy
+
 const counterProduct = document.querySelectorAll(
 	".recomendations__products-prod-couter-input"
 );
@@ -24,8 +86,6 @@ const counterMinusProduct = document.querySelectorAll(
 const counterPlusProduct = document.querySelectorAll(
 	".recomendations__products-prod-plus"
 );
-
-// slajder produktowy
 
 let count = 1;
 
